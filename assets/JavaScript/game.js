bdb45dc89164f14be057cf9a078edeb6c51a3cbd
Andre Batista <andrebatista@Andres-MacBook-Air.local>
2019-03-20 17:37:07 -0400

    HTML and CSS Styling ready, commencing JQuery

---
+++ assets/JavaScript/game.js
@@ -0,0 +1,19 @@
+<script>
+
+
+{/* trackables.crystals.diamond.element = */}
+
+
+{/* tip: create call function where you want it executed,THEN create the actual function underneath */}
+
+
+
+
+
+
+
+
+
+
+
+</script>

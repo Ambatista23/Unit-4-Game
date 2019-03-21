cbbf73a528158f1436f1da6e3d334e021b1409de
Andre Batista <andrebatista@Andres-MacBook-Air.local>
2019-03-21 00:03:04 -0400

    added random number generator to pikachu and Number cpu generates

--- assets/JavaScript/game.js
+++ assets/JavaScript/game.js
@@ -1,19 +1,23 @@
 <script>
 
+{/* 1. take each counter at a time */}
+{/*     - display random score you want to match*/}
 
-{/* trackables.crystals.diamond.element = */}
+
+
+{/*
+        - display wins and losses go up everytime
+        - display your total score from playing
+        - display sum of points you are trying to match to
+*/}
+{/* 2. random number generator
+        - random number generator for each pokemon every turn
+        - random number generator for goal number
+ */}
+        
 
 
 {/* tip: create call function where you want it executed,THEN create the actual function underneath */}
-
-
-
-
-
-
-
-
-
 
 
 </script>
